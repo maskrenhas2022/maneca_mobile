95e74745f2ddc237ba7ab5ad51828d625fc1d16f
Maskrenhas2022 <maskrenhas02@gmail.com>
2025-06-27 20:47:59 -0400

    new folders and files

--- lib/app.dart
+++ lib/app.dart
@@ -5,8 +5,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Maskrenhas Home '))),
-    );
+    return MaterialApp(home: Scaffold());
   }
 }
